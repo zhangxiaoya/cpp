759bcce18b4571abb93a5c51a70ede3220ac3a01
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-06-01 18:14:15 +0800

    add steady_clock

--- cpp11/chrono_practice/practice_clock.cpp
+++ cpp11/chrono_practice/practice_clock.cpp
@@ -22,5 +22,16 @@ int main()
     auto tomorrow = now + one_day;
     time_t tommorow_t = system_clock::to_time_t(tomorrow);
     cout << "明天是 " << ctime(&tommorow_t) << endl;
+
+
+    chrono::time_point<chrono::steady_clock, day_type> today_steady =
+            chrono::time_point_cast<day_type, chrono::steady_clock, chrono::steady_clock::duration>(
+            chrono::steady_clock::now());
+
+    cout << "稳定版本的now " << today_steady.time_since_epoch().count() << "天" << endl;
+
+    // 使用steady_clock的时候，返回的时间点不会计算到计算机元年的时间，两者的定义就不同
+    // systen_clock -- Time returned represents wall time from the system-wide clock.
+    // steady clcck -- Time returned has the property of only increasing at a uniform rate.
     return 0;
 }
